326501f1fb1b07c5f2b501e929a51b280157884f
black <black@GIDDY_CRIMSON>
2025-06-13 06:18:38 +0300

    Add project files.

---
+++ IterationAndRecursion/Program.cs
@@ -0,0 +1,15 @@
+﻿// Your Name
+// CIS 261
+// Lab 2: Iterative and Recursive Functionality
+
+static long IterativeFactorial(int n)
+{
+    long result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+
